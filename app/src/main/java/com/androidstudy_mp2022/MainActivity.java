7775ac190b0bd947a2e6df5ee099e7b77531b5c5
(Code-Jihwan) <(jihwan3194@naver.com)>
2022-09-11 23:25:02 +0900

    22.09.08 실습

--- app/src/main/java/com/androidstudy_mp2022/MainActivity.java
+++ app/src/main/java/com/androidstudy_mp2022/MainActivity.java
@@ -1,14 +1,19 @@
 package com.androidstudy_mp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+//1.화면에 한 개의 텍스트 뷰 보이기  (mp2022  p.5)   22.09.08
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World!!");
+        textView.setTextSize(50);
+        setContentView(textView);
     }
 }
